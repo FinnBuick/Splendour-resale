215419cb4be98cb1ecb5b5a8f6c63449a75af6db
Finn Buick <finnbuick@gmail.com>
2018-05-14 21:18:08 +1000

    added selenium

--- seleniumMonitor.py
+++ seleniumMonitor.py
@@ -7,15 +7,19 @@ from bs4 import BeautifulSoup
 # Import Time (to add a delay between the times the scape runs)
 import time
 
-import webbrowser
+from selenium import webdriver
 
 # This is a pretty simple script. The script downloads the homepage of VentureBeat, and if it finds some text, emails me.
 # If it does not find some text, it waits 60 seconds and downloads the homepage again.
 
+# set the url as Moshtix,
+url = "http://www.moshtix.com.au/v2/event/splendour-in-the-grass-2018/103360"
+driver = webdriver.Chrome()
+driver.get(url)
+
 # while this is true (it is true by default),
 while True:
-    # set the url as Moshtix,
-    url = "http://www.moshtix.com.au/v2/event/splendour-in-the-grass-2018/103360"
+
     # set the headers like we are a browser,
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -34,6 +38,8 @@ while True:
 
     # but if the word "Google" occurs any other number of times,
     else:
-        webbrowser.open(url)
+        select = driver.find_element(By.XPATH, '//*[@id="event-tickets-form"]/table/tbody/tr[2]/td[8]/select')
+        select.selectByIndex(1)
+        driver.find_element(By.XPATH, '//*[@id="event-buy-tickets"]').click()
 
         break
